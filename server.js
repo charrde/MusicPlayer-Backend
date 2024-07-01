e6c1a74d911b39f9d53fec7f7bc8c8bf848e0aeb
charrde <charrrde@gmail.com>
2024-07-01 04:02:38 -0600

    Added a fetch for user-info.

--- server.js
+++ server.js
@@ -87,6 +87,21 @@ app.get('/auth-check', (req, res) => {
 	}
 });
 
+app.get('/user-info', requireAuth, async (req, res) => {
+	const userId = req.auth.id
+	try {
+		const result = await pool.query('SELECT * FROM users WHERE id = $1', [userId])
+		if (result.rows.length === 0) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+		const user = result.rows[0];
+		res.json({ user });
+	}
+	catch (err) {
+		res.status(500).json({ error: 'An unexpected error occurred. Please try again later.' });
+	}
+});
+
 // Register new users
 app.post('/register', async (req, res) => {
 	const { username, password, secret } = req.body;
